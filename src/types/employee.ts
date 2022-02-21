6df532c54d285c311cc9602bc6d5f3ac8877cc74
grandorder999 <suzuo.kawabata@rakus-partners.co.jp>
2022-02-21 15:28:27 +0900

    入社日のフォーマット変更のため、編集済

--- src/types/employee.ts
+++ src/types/employee.ts
@@ -1,3 +1,4 @@
+import { format } from "date-fns";
 /**
  * 従業員情報を表すクラス.
  *
@@ -31,6 +32,14 @@ export class Employee {
     // 扶養人数
     private _dependentsCount: number
   ) {}
+
+  /**
+   * 入社日のフォーマットを"yyyy年MM月dd日"に変更して表示する.
+   * @returns "yyyy年MM月dd日"のフォーマットに変更した入社日
+   */
+  get formatHireDate(): string {
+    return format(this.hireDate, "yyyy年MM月dd日");
+  }
 
   public get id(): number {
     return this._id;
